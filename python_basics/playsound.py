c4d1a3d9ebbd357b2bd94652e312fa09935da56a
Roman-James Zeph <roman-james.zeph@student.hku.nl>
2023-09-12 14:24:26 +0200

    added playback amount

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,5 +1,8 @@
 import simpleaudio as sa
 
+playback_amount = int(input("Hoe vaak wil je het geluid horen?"))
+
 wave_obj = sa.WaveObject.from_wave_file("/Library/Audio/Samples/Sound Kits/cartoon/boing.wav")
-play_obj = wave_obj.play()
-play_obj.wait_done()
+for _ in range(playback_amount):
+	play_obj = wave_obj.play()
+	play_obj.wait_done()
