76c589481b26f295d00405b56968c175cb2107f7
Roman-James Zeph <roman-james.zeph@student.hku.nl>
2023-10-02 14:22:27 +0200

    updated eventbasedsequencer, added timestamp input and playback

--- python_basics/eventbasedsequencer.py
+++ python_basics/eventbasedsequencer.py
@@ -5,52 +5,85 @@ hihat = sa.WaveObject.from_wave_file("/Users/romanjameszeph/HKU/Jaar2/CSD2/CSD_2
 snare = sa.WaveObject.from_wave_file("/Users/romanjameszeph/HKU/Jaar2/CSD2/CSD_23-24/blok2a/assets/snare.wav")
 kick = sa.WaveObject.from_wave_file("/Users/romanjameszeph/HKU/Jaar2/CSD2/CSD_23-24/blok2a/assets/kick.wav")
 
-# hihat_event = {
-#     'timestamp': 500,
-#     'instrumentname': "hihat",
-#     'instrument': hihat,
-#     'velocity': 82,
-#     'duration': 100
-# }
+hihat_event = {
+    'timestamp': [],
+    'instrumentname': "hihat",
+    'instrument': hihat,
+    'velocity': 82,
+    'duration': 100
+}
 
-# snare_event = {
-#     'timestamp': 1000,
-#     'instrumentname': "snare",
-#     'instrument': snare,
-#     'velocity': 82,
-#     'duration': 100
-# }
+snare_event = {
+    'timestamp': [],
+    'instrumentname': "snare",
+    'instrument': snare,
+    'velocity': 82,
+    'duration': 100
+}
 
-# kick_event = {
-#     'timestamp': 0,
-#     'instrumentname': "kick",
-#     'instrument': kick,
-#     'velocity': 82,
-#     'duration': 100
-# }
+kick_event = {
+    'timestamp': [],
+    'instrumentname': "kick",
+    'instrument': kick,
+    'velocity': 82,
+    'duration': 100
+}
 
-def getTimestamp(event):
-    return event['timestamp']
+# Timestamp input for hihat
+correctInput = False
+while (not correctInput):
+    try:
+        timestamp_input = int(input("Give timestamps for hihat:"))
+        hihat_event['timestamp'].append(timestamp_input)
+    except ValueError:
+        correctInput = True
 
-events = [{'instrument': kick, 'timestamp': 0},
-          {'instrument': snare, 'timestamp': 1000},
-          {'instrument': hihat, 'timestamp': 500}]
+# Timestamp input for snare
+correctInput = False
+while (not correctInput):
+    try:
+        timestamp_input = int(input("Give timestamps for snare:"))
+        snare_event['timestamp'].append(timestamp_input)
+    except ValueError:
+        correctInput = True
 
-events.sort(key=getTimestamp)
+# Timestamp input for kick
+correctInput = False
+while (not correctInput):
+    try:
+        timestamp_input = int(input("Give timestamps for kick:"))
+        kick_event['timestamp'].append(timestamp_input)
+    except ValueError:
+        correctInput = True
 
-def handle_note_event(event):
-    if(events.get('instrument') == kick):
-        events[event].play()
-        time.sleep(events[kick].get('timestamp'))
-    if(events.get('instrument') == snare):
-        events[event].play()
-        time.sleep(events[snare].get('timestamp'))
-    if(events.get('instrument') == hihat):
-        events[event].play()
-        time.sleep(events[event].get('timestamp'))
+# fuction to play event
+def event_handler(event):
+        event['instrument'].play()
 
-handle_note_event(kick)
-handle_note_event(hihat)
-handle_note_event(snare)
+# stores all events into a list
+events = [kick_event, hihat_event, snare_event]
+
+# Creates a list of all timestamps from every event and sorts them
+all_timestamps = []
+for event in events:
+     all_timestamps.extend(event['timestamp'])
+all_timestamps.sort()
+
+# stores current time in ms
+current_time = time.time() * 1000
+
+# Iterate through each timestamp and calculates time to sleep (in seconds) for each timestamp, 
+for timestamp in all_timestamps:
+    time_to_sleep = (timestamp - current_time) / 1000.0
+    if time_to_sleep > 0:
+        time.sleep(time_to_sleep)
+    # Iterate through each event and check if the current timestamp is present in the timestamps list
+    for event in events:
+        if timestamp in event['timestamp']:
+            event_name = event['instrumentname']
+            print(f"Current Time: {timestamp / 1000} seconds, Instrument: {event_name}") 
+            event_handler(event)
+    # updates current_time to the current timestamp
+    current_time = timestamp
 
 time.sleep(1)
