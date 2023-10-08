d1995ac457517e3dda2cf1df7f1d685fa25c2524
Roman-James Zeph <roman-james.zeph@student.hku.nl>
2023-10-08 13:24:29 +0200

    updated eventbasedsequencer, added note duration input

--- python_basics/eventbasedsequencer.py
+++ python_basics/eventbasedsequencer.py
@@ -12,7 +12,9 @@ kick = sa.WaveObject.from_wave_file(
 )
 
 hihat_event = {
-    "timestamp": [],
+    "noteDurations": [],
+    "timestamps16th": [0.0],
+    "timestampsMs": [],
     "instrumentname": "hihat",
     "instrument": hihat,
     "velocity": 82,
@@ -20,7 +22,9 @@ hihat_event = {
 }
 
 snare_event = {
-    "timestamp": [],
+    "noteDurations": [],
+    "timestamps16th": [0.0],
+    "timestampsMs": [],
     "instrumentname": "snare",
     "instrument": snare,
     "velocity": 82,
@@ -28,70 +32,99 @@ snare_event = {
 }
 
 kick_event = {
-    "timestamp": [],
+    "noteDurations": [],
+    "timestamps16th": [0.0],
+    "timestampsMs": [],
     "instrumentname": "kick",
     "instrument": kick,
     "velocity": 82,
     "duration": 100,
 }
 
-# Timestamp input for hihat
-correctInput = False
-while not correctInput:
-    try:
-        timestamp_input = float(input("Give timestamps for hihat:"))
-        hihat_event["timestamp"].append(timestamp_input)
-    except ValueError:
-        correctInput = True
+# stores all events in list
+events = [kick_event, hihat_event, snare_event]
 
-# Timestamp input for snare
-correctInput = False
-while not correctInput:
-    try:
-        timestamp_input = float(input("Give timestamps for snare:"))
-        snare_event["timestamp"].append(timestamp_input)
-    except ValueError:
-        correctInput = True
+# bpm input
+try:
+    bpm = float(
+        input("Standard BPM is 100, leave empty to continue or type your own BPM")
+    )
+except ValueError:
+    bpm = 100
 
-# Timestamp input for kick
-correctInput = False
-while not correctInput:
-    try:
-        timestamp_input = float(input("Give timestamps for kick:"))
-        kick_event["timestamp"].append(timestamp_input)
-    except ValueError:
-        correctInput = True
+# note duration input
+for event in events:
+    correctInput = False
+    while not correctInput:
+        try:
+            event_name = event['instrumentname']
+            noteDurationInput = float(input(f"Give note duration for {event_name}"))
+            event["noteDurations"].append(noteDurationInput)
+        except ValueError:
+            print(event['instrumentname'], "note durations:")
+            print(event["noteDurations"])
+            correctInput = True
+print("\n")
 
+# note duration to timestamps16th
+for event in events:
+    for noteDuration in event["noteDurations"]:
+        event["timestamps16th"].append(event["timestamps16th"][-1] + noteDuration * 4)
+    event["timestamps16th"].pop(0)
+    print(event["instrumentname"], "timestamps in 16th:")
+    print(event["timestamps16th"])
+print("\n")
+
+# Calculates sixteenth note duration
+sixteenthNoteDuration = 15.0 / bpm
+
+# timestamps16th to timestampsMs
+for event in events:
+    for timestamp16th in event["timestamps16th"]:
+        event["timestampsMs"].append(timestamp16th * sixteenthNoteDuration)
+    print(event["instrumentname"], "timestamps in ms")
+    print(event["timestampsMs"])
+print("\n")
 
 # fuction to play event
 def event_handler(event):
     event["instrument"].play()
 
-
-# stores all events into a list
-events = [kick_event, hihat_event, snare_event]
-
 # Creates a list of all timestamps from every event and sorts them
 all_timestamps = []
 for event in events:
-    all_timestamps.extend(event["timestamp"])
+    all_timestamps.extend(event["timestampsMs"])
 all_timestamps.sort()
 
 # stores current time in ms
-current_time = time.time() * 1000
+current_time = time.time()
 
-# Iterate through each timestamp and calculates time to sleep (in seconds) for each timestamp,
+# Initialize a dictionary to keep track of played events
+played_events = {}
+
+# Iterate through each timestamp and calculate time to sleep
 for timestamp in all_timestamps:
-    time_to_sleep = (timestamp - current_time) / 1000.0
+    time_to_sleep = timestamp - current_time
+
     if time_to_sleep > 0:
         time.sleep(time_to_sleep)
+
     # Iterate through each event and check if the current timestamp is present in the timestamps list
     for event in events:
-        if timestamp in event["timestamp"]:
+        if timestamp in event["timestampsMs"]:
             event_name = event["instrumentname"]
-            print(f"Current Time: {timestamp / 1000} seconds, Instrument: {event_name}")
-            event_handler(event)
-    # updates current_time to the current timestamp
+            
+            # Check if this event has already been played at this timestamp
+            if event_name not in played_events or timestamp not in played_events[event_name]:
+                print(f"Current Time: {timestamp} seconds, Instrument: {event_name}")
+                event_handler(event)
+                
+                # Mark the event as played at this timestamp
+                if event_name not in played_events:
+                    played_events[event_name] = set()
+                played_events[event_name].add(timestamp)
+
+    # Update current_time to the current timestamp
     current_time = timestamp
 
 time.sleep(1)
