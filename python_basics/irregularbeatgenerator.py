77bf55d8e42182df10d8fbdf5d1d54847b702a50
Roman-James Zeph <roman-james.zeph@student.hku.nl>
2023-10-23 22:57:24 +0200

    updated irregularbeatgenerator, added comments, added prints, cleanup

--- python_basics/irregularbeatgenerator.py
+++ python_basics/irregularbeatgenerator.py
@@ -5,21 +5,21 @@ import os
 from midiutil import MIDIFile
 
 def customInput():
-    # bpm input
+    # Bpm input
     try:
-        bpm = float(input("Standard BPM is 100, leave empty to continue or type your own BPM"))
+        bpm = float(input("What BPM do you want? (default = 120)\nYour choice:"))
     except ValueError:
-        bpm = 200
+        bpm = 120
     
-    # measures input
+    # Measures input
     try:
-        measures = float(input("How many measures do you want?"))
+        measures = int(input("How many measures do you want? (default = 4)\nYour choice:"))
     except ValueError:
-        measures = 2
+        measures = 4
 
-    # timesignature input
+    # Timesignature input
     try:
-        time_signature_str = input("Enter the time signature (e.g., 4/4): ")
+        time_signature_str = input("What time signature do you want? (default = 4/4)\nYour choice:")
         time_signature_parts = time_signature_str.split('/')
         numerator = int(time_signature_parts[0])
         denominator = int(time_signature_parts[1])
@@ -27,7 +27,7 @@ def customInput():
         numerator = 4
         denominator = 4
 
-    # samples input
+    # Filepaths options for the different samples
     sample_options = {
         '1': {
             'hihat_path': "/Users/romanjameszeph/HKU/Jaar2/CSD2/python_basics/assets/acoustic_hat.wav",
@@ -46,184 +46,228 @@ def customInput():
         }
     }
 
-    # prompts the user to choose form the sample options
-    user_input = ''
-    while user_input not in sample_options:
-        user_input = input("Pick an option:\n1) Acoustic\n2) Boombap\n3) Trap\nYour choice: ")
+    # Prompts the user to choose from the sample options
+    sample_choice = input("Which samples do you want? (default = 1)\n1) Acoustic\n2) Lofi\n3) Trap\nYour choice: ")
+    if sample_choice not in ["1", "2", "3"]:
+        sample_choice = "1"
 
-    # retrieves the sample paths based on userinput
-    hihat_path = sample_options[user_input]['hihat_path']
-    snare_path = sample_options[user_input]['snare_path']
-    kick_path = sample_options[user_input]['kick_path']
+    # Retrieves the filepaths based on userinput
+    hihat_path = sample_options[sample_choice]['hihat_path']
+    snare_path = sample_options[sample_choice]['snare_path']
+    kick_path = sample_options[sample_choice]['kick_path']
         
-
     return bpm, measures, numerator, denominator, hihat_path, snare_path, kick_path
 
 def loadSamples(hihat_path, snare_path, kick_path):
+    # Loads each samples based on file path
     hihat = sa.WaveObject.from_wave_file(hihat_path)
     snare = sa.WaveObject.from_wave_file(snare_path)
     kick = sa.WaveObject.from_wave_file(kick_path)
     return hihat, snare, kick
 
 def defineTracks(hihat, snare, kick):
-    high_track = {
-        "noteDurations": [],
-        "timestamps16th": [0.0],
-        "timestampsMs": [],
-        "instrumentname": "hihat",
+    # Defines high_track dictionary
+    high_track = { 
         "instrument": hihat,
+        "instrumentname": "hihat",
+        "noteDurations": [],
+        "timestamps16th": [0],
+        "timestampsMs": [],
         "velocity": 82,
-        "duration": 100,
         "midi_pitch": 41,
         "note_offset": 0
     }
-
+    # defines mid_track dictionary, withrandom 1 or 2 quarter notes as snare offset
+    snare_offset = random.choice([1, 2])
     mid_track = {
-        "noteDurations": [],
-        "timestamps16th": [0.0],
-        "timestampsMs": [],
-        "instrumentname": "snare",
         "instrument": snare,
+        "instrumentname": "snare",
+        "noteDurations": [],
+        # times 4 to account for 16th notes
+        "timestamps16th": [snare_offset * 4],
+        "timestampsMs": [],
         "velocity": 82,
-        "duration": 100,
         "midi_pitch": 38,
-        "note_offset": 1
+        "note_offset": snare_offset
     }
 
+    # defines low_track dictionary
     low_track = {
-        "noteDurations": [],
-        "timestamps16th": [0.0],
-        "timestampsMs": [],
-        "instrumentname": "kick",
         "instrument": kick,
+        "instrumentname": "kick",
+        "noteDurations": [],
+        "timestamps16th": [0],
+        "timestampsMs": [],
         "velocity": 82,
-        "duration": 100,
         "midi_pitch": 35,
         "note_offset": 0
     }
 
-    # stores all tracks in events list
-    events = [high_track, mid_track, low_track]
+    # stores all tracks in tracks list
+    tracks = [high_track, mid_track, low_track]
 
-    return events
+    return tracks
 
-def rythmGeneration(events, measures, numerator, denominator):
-    for event in events:
-        # Adjust note durations based on the time signature
-        num_pulses = numerator * measures
-        while num_pulses:
-            # Add 2's and 3's in a random manner
-            if num_pulses >= 3:
-                dur = random.randint(2, 3)
-                num_pulses -= dur
-                event["noteDurations"].append(dur)
-            else:
-                if num_pulses == 2:
-                    event["noteDurations"].append(2)
-                    num_pulses -= 2
-                elif num_pulses == 1:
-                    if event["noteDurations"][-1] == 2:
-                        event["noteDurations"][-1] += 1  # Extend the last note with +1
-                    else:
-                        event["noteDurations"][-1] -= 1  # Change the last note into a 2 and add a 2
-                        event["noteDurations"].append(2)
-                    num_pulses -= 1
+def baseRhythmGeneration(measures, numerator):
+    # creates an empty list to store base sequence
+    base_seq = []
 
-    for event in events:
-        print(event["instrumentname"], "notedurations:")
-        print(event["noteDurations"])
-        print("\n")
+    # Adjust number of pulses based on the numerator times measures
+    num_pulses = numerator * measures
 
-    return events
+    while num_pulses:
+        # Add 2's and 3's in a random manner
+        if num_pulses >= 3:
+            dur = random.randint(2, 3)
+            num_pulses -= dur
+            base_seq.append(dur)
+        else:
+            if num_pulses == 2:
+                base_seq.append(2)
+                num_pulses -= 2
+            elif num_pulses == 1:
+                if base_seq[-1] == 2:
+                    base_seq[-1] += 1  # Extend the last note with +1
+                else:
+                    base_seq[-1] -= 1  # Change the last note into a 2 and add a 2
+                    base_seq.append(2)
+                num_pulses -= 1
+    return base_seq
 
-def noteDurationToTimestamps16th(events):
-    for event in events:
-        for noteDuration in event["noteDurations"]:
-            event["timestamps16th"].append(event["timestamps16th"][-1] + noteDuration * 4)
-        event["timestamps16th"].pop(0)
-        print(event["instrumentname"], "timestamps in 16th:")
-        print(event["timestamps16th"])
-    print("\n")
-    return events
+def noteDurationFiller(tracks, base_seq):
+    for track in tracks:
+        if track["instrumentname"] == "hihat":
+            for note in base_seq * 4:
+                # Fills the note durations of the hihat to be four times faster than those in the base sequence
+                track["noteDurations"].append(note / 4)
 
-def timestamps16thToTimestampsMs(events, bpm):
-    sixteenthNoteDuration = 15.0 / bpm
-    for event in events:
-        for timestamp16th in event["timestamps16th"]:
-            event["timestampsMs"].append(timestamp16th * sixteenthNoteDuration)
-        print(event["instrumentname"], "timestamps in ms")
-        print(event["timestampsMs"])
-    print("\n")
-    return events
+        if track["instrumentname"] == "snare": 
+            for note in base_seq:
+                # Applies the base sequence to the snare's note durations
+                track["noteDurations"].append(note)
+            # Adjusts the final note by subtracting 1 or 2 beats based on the note offset.
+            if track["note_offset"] == 1:
+                track["noteDurations"][-1] -= 1
+            elif track["note_offset"] == 2:
+                track["noteDurations"][-1] -= 2
 
-def allTimestampsList(events):
+        if track["instrumentname"] == "kick":
+            for note in base_seq * 2:
+                # fills the note durations of the kick to be two times faster than those in the base sequence
+                track["noteDurations"].append(note/2)
+
+    return tracks
+
+def noteDurationToTimestamps16th(tracks):
+    # Calculate timestamps in 16th notes for each track based on note durations (times 4 to account for 16th notes)
+    for track in tracks:
+        for noteDuration in track["noteDurations"]:
+            track["timestamps16th"].append(track["timestamps16th"][-1] + noteDuration * 4)
+
+    # removes same 16th timestamps as the snare from the kick
+    for ts in tracks[1]["timestamps16th"]:
+        if ts in tracks[2]["timestamps16th"]:
+            tracks[2]["timestamps16th"].remove(ts)
+
+    return tracks
+
+def timestamps16thToTimestampsMs(tracks, bpm):
+    # Calculates sixteenth note duration in miliseconds
+    sixteenthNoteDuration = (60/4) / bpm
+
+    # Convert 16th note timestamps to milliseconds for each track
+    for track in tracks:
+        for timestamp16th in track["timestamps16th"]:
+            track["timestampsMs"].append(timestamp16th * sixteenthNoteDuration)
+
+    return tracks
+
+def allTimestampsList(tracks):
+    # Creates an empty list to store all timestamps
     all_timestamps = []
-    for event in events:
-        all_timestamps.extend(event["timestampsMs"])
+
+    # fills the all_timestamps list with all the timestamps in ms from each track
+    for track in tracks:
+        all_timestamps.extend(track["timestampsMs"])
+    
+    # Sorts the all_timestamps list
     all_timestamps.sort()
+
     return all_timestamps
 
-def iterateThroughTimestamps(events, all_timestamps):
+def iterateThroughTimestamps(tracks, all_timestamps):
     # Get the start time
     time_zero = time.time()
 
-    # Create a dictionary to keep track of played events for each timestamp
-    played_events = {ts: [] for ts in all_timestamps}
+    # Create a dictionary to keep track of played tracks for each timestamp
+    played_timestamps = {ts: [] for ts in all_timestamps}
+
+    # prints "♪ Playing Your Beat ♪" to indicate to the user that the beat is playing
+    print("♪ Playing Your Beat ♪")
 
     while all_timestamps:
         now = time.time() - time_zero
         ts = all_timestamps[0]
 
         if now >= ts:
-            # Find the corresponding event and play it if it hasn't been played yet
-            for event in events:
-                if ts in event["timestampsMs"] and event["instrumentname"] not in played_events[ts]:
-                    event_name = event["instrumentname"]
-                    # print(f"Current Time: {ts} seconds, Instrument: {event_name}")
-                    event["instrument"].play()
-                    played_events[ts].append(event_name)
+            # Find the corresponding timestamp and play it if it hasn't been played yet
+            for track in tracks:
+                if ts in track["timestampsMs"] and track["instrumentname"] not in played_timestamps[ts]:
+                    track_name = track["instrumentname"]
+                    track["instrument"].play()
+                    played_timestamps[ts].append(track_name)
             all_timestamps.pop(0)
         time.sleep(0.001)
 
-def durationToMidi(events, bpm):
-    # create the MIDIfile object, to which we can add notes
+    # Print "Done playing your beat" to indicate to the user that the beat is done playing
+    print("Done playing your beat")
+
+def durationToMidi(tracks, bpm):
+    # Create the MIDIfile object, to which we can add notes
     mf = MIDIFile(1)
 
-    track = 0
-    channel = 9  # corresponds to channel 10 drums
+    # Set name and tempo
+    mf.addTrackName(0, 0, "Beat Sample Track")
+    mf.addTempo(0, 0, bpm)
 
-    # set name and tempo
-    time_beginning = 0
-    mf.addTrackName(track, time_beginning, "Beat Sample Track")
-    mf.addTempo(track, time_beginning, bpm)
-
-    # add the notes for the different tracks
-    for event in events:
-        time = event["note_offset"]
-        for noteDuration in event["noteDurations"]:
-            mf.addNote(track, channel, event["midi_pitch"], time, noteDuration, event["velocity"])
+    # Adds the notes from each track to the MIDIFile
+    for track in tracks:
+        time = track["note_offset"]
+        for noteDuration in track["noteDurations"]:
+            mf.addNote(0, 9, track["midi_pitch"], time, noteDuration, track["velocity"])
             # increment time based on the duration of the added note
             time = time + noteDuration
-    
-    # specifies the output path
-    output = os.path.abspath("/Users/romanjameszeph/HKU/Jaar2/CSD2/python_basics/exports/coolrythm.midi")
 
+    # Specifies the output path
+    output = os.path.abspath("/Users/romanjameszeph/HKU/Jaar2/CSD2/python_basics/exports/beat.mid")
+
+    # Writes midi file to output path
     with open(output,'wb') as output_file:
         mf.writeFile(output_file)
 
+    # Prints output location of the created midifile
+    print('Created midifile in:', output)
+
 def main():
     onOff = True;
+    saveToMidi = True;
+    print ("Welcome to Roman-James's Beatmaker!\n")
     while onOff:
         bpm, measures, numerator, denominator, hihat_path, snare_path, kick_path = customInput()
         hihat, snare, kick = loadSamples(hihat_path, snare_path, kick_path)
-        events = defineTracks(hihat, snare, kick)
-        events = rythmGeneration(events, measures, numerator, denominator)
-        events = noteDurationToTimestamps16th(events)
-        events = timestamps16thToTimestampsMs(events, bpm)
-        all_timestamps = allTimestampsList(events)
-        iterateThroughTimestamps(events, all_timestamps)
-        durationToMidi(events, bpm)
-        onOff = input("do you want to continue? Press y and enter: ") == 'y'
+        tracks = defineTracks(hihat, snare, kick)
+        base_seq = baseRhythmGeneration(measures, numerator)
+        tracks = noteDurationFiller(tracks, base_seq)
+        tracks = noteDurationToTimestamps16th(tracks)
+        tracks = timestamps16thToTimestampsMs(tracks, bpm)
+        all_timestamps = allTimestampsList(tracks)
+        iterateThroughTimestamps(tracks, all_timestamps)
+        saveToMidi = input("Do you want to save your beat to midi? Press y and enter: ") == 'y'
+        if saveToMidi:
+            durationToMidi(tracks, bpm)
+        onOff = input("Do you want to create another beat? Press y and enter: ") == 'y'
+    print("Quiting program..")
+
 
 if __name__ == "__main__":
     main()
