9fd3c5f3c91c54e261f3afa78859f8a19dc7c1f6
Roman-James Zeph <roman-james.zeph@student.hku.nl>
2023-11-14 16:06:35 +0100

    added C++_basics

---
+++ C++_basics/helloworld.cpp
@@ -0,0 +1,21 @@
+/*
+ * Super basic C++ hello world
+ *
+ * ... but hey.. what's the purpose of hello world?
+ *
+ * ... that's right.. being super basic!
+ *
+ * Save this file as helloworld.cpp
+ *
+ * To run it, type the following lines on the command line:
+ *  make helloworld
+ *  ./helloworld
+ *
+ */
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World :-)" << std::endl;
+  return 0;
+}
