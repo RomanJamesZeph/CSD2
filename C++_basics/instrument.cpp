0200d7754a9e710aa47df63feded7a2c66a4d997
Roman-James Zeph <roman-james.zeph@student.hku.nl>
2023-11-19 15:37:58 +0100

    updated instrument.cpp (2 methods)

--- C++_basics/instrument.cpp
+++ C++_basics/instrument.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <string>
 
+// class Instrument{
+//     public:
+//         Instrument(std::string sound){
+//             std::cout << sound << std::endl;
+//         }
+// };
+
+// int main (){
+//     Instrument myInstrument1("Bazinga");
+    
+//     Instrument myInstrument2("Tjoooep");
+// }
+
 class Instrument{
     public:
-        void play (std::string sound)
-        {
+        std::string sound;
+        void play(){
             std::cout << sound << std::endl;
         }
 };
 
 int main (){
     Instrument myInstrument1;
-    myInstrument1.play("Bazinga");
+    myInstrument1.sound = ("Bazinga");
+    myInstrument1.play();
+
     
     Instrument myInstrument2;
-    myInstrument2.play("Tjoooep");
+    myInstrument2.sound = ("Tjoooep");
+    myInstrument2.play();
 }
